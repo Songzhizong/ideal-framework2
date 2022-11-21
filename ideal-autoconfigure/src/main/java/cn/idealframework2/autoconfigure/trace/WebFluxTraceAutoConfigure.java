7ff231b88b0c564ae4caad293e7f0f1061ff2961
宋志宗 <zzsong91@163.com>
2022-11-21 16:29:24 +0800

    未配置OperatorHolder或OperationLogStore时输出警告日志

--- ideal-autoconfigure/src/main/java/cn/idealframework2/autoconfigure/trace/WebFluxTraceAutoConfigure.java
+++ ideal-autoconfigure/src/main/java/cn/idealframework2/autoconfigure/trace/WebFluxTraceAutoConfigure.java
@@ -5,6 +5,8 @@ import cn.idealframework2.starter.model.trace.coroutine.CoroutineTraceModel;
 import cn.idealframework2.trace.reactive.OperationLogStore;
 import cn.idealframework2.trace.reactive.OperatorHolder;
 import cn.idealframework2.trace.reactive.TraceFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,6 +24,7 @@ import java.util.Set;
  */
 @ConditionalOnClass({CoroutineTraceModel.class, WebFilter.class})
 public class WebFluxTraceAutoConfigure {
+  private static final Logger log = LoggerFactory.getLogger(WebFluxTraceAutoConfigure.class);
 
   @Value("${spring.application.name:}")
   private String applicationName;
@@ -34,6 +37,12 @@ public class WebFluxTraceAutoConfigure {
                                  OperationLogStore operationLogStore,
                                  @Nonnull @Qualifier("requestMappingHandlerMapping")
                                  RequestMappingHandlerMapping handlerMapping) {
+    if (operatorHolder == null) {
+      log.warn("未配置OperatorHolder, 无法记录操作日志");
+    }
+    if (operationLogStore == null) {
+      log.warn("未配置OperationLogStore, 无法记录操作日志");
+    }
     String system = properties.getSystem();
     Set<String> excludePatterns = properties.getExcludePatterns();
     if (StringUtils.isBlank(system) && StringUtils.isNotBlank(applicationName)) {
