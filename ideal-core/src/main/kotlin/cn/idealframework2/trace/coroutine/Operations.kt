b0b2cf4ab6efd9918a76415ce7137491f4910e42
宋志宗 <zzsong91@163.com>
2022-11-30 19:24:59 +0800

    为Operations添加新的方法

--- ideal-core/src/main/kotlin/cn/idealframework2/trace/coroutine/Operations.kt
+++ ideal-core/src/main/kotlin/cn/idealframework2/trace/coroutine/Operations.kt
@@ -5,12 +5,19 @@ import cn.idealframework2.trace.OperationLog
 /**
  * @author 宋志宗 on 2022/9/24
  */
+@Suppress("unused")
 object Operations {
 
   suspend fun details(details: String) {
     val context = TraceContextHolder.awaitContext() ?: return
     val operationLog = context.operationLog ?: return
     operationLog.details = details
+  }
+
+  suspend inline fun details(block: () -> String) {
+    val context = TraceContextHolder.awaitContext() ?: return
+    val operationLog = context.operationLog ?: return
+    operationLog.details = block.invoke()
   }
 
   suspend fun failure(message: String) {
@@ -20,12 +27,21 @@ object Operations {
     operationLog.message = message
   }
 
+  suspend inline fun failure(block: () -> String) {
+    val context = TraceContextHolder.awaitContext() ?: return
+    val operationLog = context.operationLog ?: return
+    operationLog.isSuccess = false
+    operationLog.message = block.invoke()
+  }
+
+  /** 设置变更前的信息 */
   suspend fun before(before: String) {
     val context = TraceContextHolder.awaitContext() ?: return
     val operationLog = context.operationLog ?: return
     operationLog.before = before
   }
 
+  /** 设置变更后的信息 */
   suspend fun after(after: String) {
     val context = TraceContextHolder.awaitContext() ?: return
     val operationLog = context.operationLog ?: return
@@ -34,5 +50,11 @@ object Operations {
 
   suspend fun log(): OperationLog? {
     return TraceContextHolder.awaitContext()?.operationLog
+  }
+
+  suspend inline fun log(block: (OperationLog) -> Unit) {
+    val context = TraceContextHolder.awaitContext() ?: return
+    val operationLog = context.operationLog ?: return
+    block.invoke(operationLog)
   }
 }
